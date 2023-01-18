aa9732b1f50f04ac73b8168b3571f76cd6758d95
Joao Paulo <oliveirajpufrj@gmail.com>
2023-01-18 14:35:47 -0300

    ex completo

--- src/main/java/com/carsapi/api/controller/CarController.java
+++ src/main/java/com/carsapi/api/controller/CarController.java
@@ -1,23 +1,33 @@
 package com.carsapi.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carsapi.api.dto.CarDTO;
+import com.carsapi.api.model.Car;
+import com.carsapi.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/api/cars")
 public class CarController {
     
-    @PostMapping()
-    public void postCar(@RequestBody CarDTO req){
-        System.out.println("Todos os dados:");
-        System.out.println(req);
-        System.out.println("\n");
-        System.out.println("Modelo e ano:");
-        System.out.println(req.modelo() + " " + req.anoModelo());
-        System.out.println("\n");
+    @Autowired
+    private CarRepository repository;
+
+    @PostMapping
+    public Object create(@RequestBody CarDTO req){
+      repository.save(new Car(req));
+      return req;
+    }
+
+    @GetMapping
+    public List<Car> listAll(){
+      return repository.findAll();
     }
 }
